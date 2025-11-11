f9b053660501061ec5ec840941722b76555b4cbd
Aydin-Fatih-Emre <fatih.aydin.1@study.thws.de>
2025-11-11 16:01:23 +0100

    Kalender updar

--- ReMindHealth/Components/Pages/Kalender.razor.cs
+++ ReMindHealth/Components/Pages/Kalender.razor.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Components;
+using System.Globalization;
 
 namespace ReMindHealth.Components.Pages
 {
@@ -8,18 +9,21 @@ namespace ReMindHealth.Components.Pages
 
         private List<Termin> termine = new()
         {
-            new Termin { Id = 1, Titel = "Dr. Schmidt", Datum = new DateTime(2025, 10, 23), Uhrzeit = "14:30" },
-            new Termin { Id = 2, Titel = "Laboruntersuchung", Datum = new DateTime(2025, 12, 03), Uhrzeit = "09:00" }
+            new Termin { Id = 1, Titel = "Dr. Schmidt", Datum = new DateTime(2025, 10, 20), Uhrzeit = "14:30", Adresse = "Blumenstraße 3, 92120 Nürnberg" },
+            new Termin { Id = 2, Titel = "", Datum = new DateTime(2025, 10, 23), Uhrzeit = "09:00", Adresse = "" },
+            new Termin { Id = 3, Titel = "Laboruntersuchung", Datum = new DateTime(2025, 12, 03), Uhrzeit = "08:15", Adresse = "Krankenhausstraße 7, 97070 Würzburg" }
         };
+
+        private Termin? selectedTermin;
+
+        private void SelectTermin(Termin termin)
+        {
+            selectedTermin = termin;
+        }
 
         private void NavigateTo(string url)
         {
             NavigationManager.NavigateTo(url);
-        }
-
-        private void NavigateToDetails(int id)
-        {
-            NavigationManager.NavigateTo($"/termin-details/{id}");
         }
 
         public class Termin
@@ -28,6 +32,8 @@ namespace ReMindHealth.Components.Pages
             public string Titel { get; set; } = "";
             public DateTime Datum { get; set; }
             public string Uhrzeit { get; set; } = "";
+            public string Adresse { get; set; } = "";
         }
     }
 }
+
